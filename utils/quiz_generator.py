091e42bb7762f47c8fd5f87111b45cea36754765
Дарья <161309114+darya78874@users.noreply.github.com>
2025-01-11 11:29:26 +0300

    Update quiz_generator.py

--- utils/quiz_generator.py
+++ utils/quiz_generator.py
@@ -1,1 +1,12 @@
+import random
+
+class QuizGenerator:
+
+    def __init__(self, words):
+
+        self.words = words
+
+    def generate_quiz(self):
+
+        return random.sample(self.words, k=min(3, len(self.words)))
 
