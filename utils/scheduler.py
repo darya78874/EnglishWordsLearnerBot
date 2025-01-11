4416e0c28b1f23c34679ca0e1ea3557274d1f556
Дарья <161309114+darya78874@users.noreply.github.com>
2025-01-11 11:28:55 +0300

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,63 +1,25 @@
-import asyncio
-import asyncpg
 import schedule
-from datetime import datetime
-from utils.parser import get_price_and_image_from_ozon
 
-async def get_db_connection():
-    return await asyncpg.connect(user='postgres', password='qptz23', database='bot', host='localhost')
+import time
 
-async def record_price_change(product_id: int, price: float):
-    conn = await get_db_connection()
-    timestamp = datetime.now()
+import threading
 
-    row = await conn.fetchrow("""
-        SELECT price
-        FROM price_history
-        WHERE product_id = $1
-        ORDER BY timestamp ASC
-        LIMIT 1
-    """, product_id)
+def job():
 
-    if not row:
-        product_row = await conn.fetchrow("""
-            SELECT price
-            FROM products
-            WHERE id = $1
-        """, product_id)
+    print("Это сообщение выдано по расписанию!")
 
-        if product_row:
-            price = product_row['price']
+def run_scheduler():
 
-    await conn.execute("""
-        INSERT INTO price_history (product_id, price, timestamp) 
-        VALUES ($1, $2, $3)
-    """, product_id, price, timestamp)
-    await conn.close()
+    schedule.every().day.at("10:00").do(job)
 
-async def price_update_task():
-    conn = await get_db_connection()
-    rows = await conn.fetch("SELECT id, url FROM products")
+    while True:
 
-    for row in rows:
-        product_id = row['id']
-        url = row['url']
-        price, img_url, name = await get_price_and_image_from_ozon(url)
+        schedule.run_pending()
 
-        if price is not None:
-            await record_price_change(product_id, price)
+        time.sleep(1)
 
-    await conn.close()
+def start_scheduler():
 
-def scheduler_setup():
-    def job():
-        asyncio.ensure_future(price_update_task())
+    thread = threading.Thread(target=run_scheduler)
 
-    schedule.every(10).minutes.do(job)
-
-    async def scheduler():
-        while True:
-            schedule.run_pending()
-            await asyncio.sleep(1)
-
-    asyncio.create_task(scheduler())
+    thread.start()
