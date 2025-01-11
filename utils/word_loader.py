ff7f9df1db07ec61a5e1d10f0e2e9d86bc80ec03
Дарья <161309114+darya78874@users.noreply.github.com>
2025-01-11 11:30:04 +0300

    Update word_loader.py

--- utils/word_loader.py
+++ utils/word_loader.py
@@ -1,1 +1,16 @@
+class WordLoader:
+
+    def __init__(self):
+
+        self.words = []
+
+    def load_words(self):
+
+        # В реальном приложении, возможно, загрузка слов из базы данных или API
+
+        return self.words
+
+    def add_word(self, word):
+
+        self.words.append(word)
 
